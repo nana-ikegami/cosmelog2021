b3b848e941fa0cf7d44128b735989bdca6ec9fd8
nana-ikegami <80028527+nana-ikegami@users.noreply.github.com>
2021-04-03 11:25:38 +0900

    コントローラー記述

--- app/controllers/cosmes_controller.rb
+++ app/controllers/cosmes_controller.rb
@@ -8,5 +8,16 @@ class CosmesController < ApplicationController
   end
 
   def create
+    @cosme = Cosme.new(cosme_params)
+    if @cosme.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  private
+  def tweet_params
+    params.require(:cosme).permit(:name, :image, :text, :title).merge(user_id: current_user.id)
   end
 end
