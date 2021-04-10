64d097889b65b18e3950b9190418f28d4e585005
nana-ikegami <80028527+nana-ikegami@users.noreply.github.com>
2021-04-10 12:13:27 +0900

    コメントコントローラーテストコード

---
+++ spec/requests/comments_spec.rb
@@ -0,0 +1,18 @@
+require 'rails_helper'
+
+RSpec.describe "Comments", type: :request do
+  before do
+    @comment = FactoryBot.create(:cosme)
+  end
+    
+  describe 'GET #show' do
+    it 'showアクションにリクエストすると正常にレスポンスが返ってくる' do 
+      get cosme_path(@comment)
+      expect(response.status).to eq 200
+    end
+    it 'showアクションにリクエストするとレスポンスに投稿済みのコスメのコメントが存在する' do 
+      get cosme_path(@comment)
+      expect(response.body).to include(@comment.text)
+    end
+  end
+end
